939774e2eb7b8fb5874fa42365d25fca93a3ec9d
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2023-09-09 16:54:58 +0700

    fix das

--- resources/views/pages/admin/dashboard/index.blade.php
+++ resources/views/pages/admin/dashboard/index.blade.php
@@ -11,13 +11,12 @@ Dashboard
             <div class="d-flex align-items-end row">
                 <div class="col-sm-7">
                     <div class="card-body">
-                        <h5 class="card-title text-primary">Congratulations John! 🎉</h5>
+                        <h5 class="card-title text-primary">Selamat Datang! 🎉</h5>
                         <p class="mb-4">
-                            You have done <span class="fw-bold">72%</span> more sales today. Check your new badge in
-                            your profile.
+                            Selamat datang, di sistem untuk mengelola data Company Profile
                         </p>
 
-                        <a href="javascript:;" class="btn btn-sm btn-outline-primary">View Badges</a>
+                        <a href="/" class="btn btn-sm btn-outline-primary">Lihat Web</a>
                     </div>
                 </div>
                 <div class="col-sm-5 text-center text-sm-left">
